094caacc796bf6173214d2611f110d7348eeebaa
Arnaud Leroy <dropthehq@gmail.com>
2026-03-29 14:16:23 +0100

    Add .gitignore, remove build artifacts from tracking

--- target/debug/deps/coffeetrove-72e6197ee3b72b78.d
+++
@@ -1,7 +0,0 @@
-/private/tmp/rust-rewrite/coffeetrove/target/debug/deps/coffeetrove-72e6197ee3b72b78.d: src/lib.rs
-
-/private/tmp/rust-rewrite/coffeetrove/target/debug/deps/libcoffeetrove-72e6197ee3b72b78.rlib: src/lib.rs
-
-/private/tmp/rust-rewrite/coffeetrove/target/debug/deps/libcoffeetrove-72e6197ee3b72b78.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/coffeetrove-ede048bba3839387.d
+++
@@ -1,5 +0,0 @@
-/private/tmp/rust-rewrite/coffeetrove/target/debug/deps/coffeetrove-ede048bba3839387.d: src/lib.rs
-
-/private/tmp/rust-rewrite/coffeetrove/target/debug/deps/libcoffeetrove-ede048bba3839387.rmeta: src/lib.rs
-
-src/lib.rs:
